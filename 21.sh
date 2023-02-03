a4a3696a5baad56973662db5ff1e4633d44f4c4f
777388 <96343159+777388@users.noreply.github.com>
2023-02-03 00:58:39 -0800

    Update 21.sh

--- 21.sh
+++ 21.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+declare -A seen_ips
+
 while read line; do
   ip=$(echo $line | awk '{print $2}')
-  ftp_response=$(ftp -v -n $ip <<END_SCRIPT
-  user anonymous anonymous
-  quit
+  if [[ -z "${seen_ips[$ip]}" ]]; then
+    ftp_response=$(ftp -v -n $ip <<END_SCRIPT
+    user anonymous anonymous
+    quit
 END_SCRIPT
 )
-  if echo $ftp_response | grep "230 Login successful" > /dev/null; then
-    echo "$ip allows anonymous FTP login"
-  else
-    echo "$ip does not allow anonymous FTP login"
+    if echo $ftp_response | grep "230 Login successful" > /dev/null; then
+      echo "$ip allows anonymous FTP login"
+    else
+      echo "$ip does not allow anonymous FTP login"
+    fi
+    seen_ips[$ip]=1
   fi
 done < port_21.txt
